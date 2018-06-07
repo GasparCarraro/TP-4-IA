921d53cc6e34c3d6b281a0d6028eb06aa4a13626
GasparCarraro <gaspar_carraro@hotmail.com>
2018-06-07 11:07:31 -0300

    Normalizacion

--- TP 4 Random Forest.py
+++ TP 4 Random Forest.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -10,10 +11,10 @@ df_data_train = pd.read_csv('train.csv', header=0)
 
 
 # Cantidad total de filas
-print("Cantidad total de filas TRAIN %s" % len(df_data_train))
+# print("Cantidad total de filas TRAIN %s" % len(df_data_train))
 #print("Cantidad total de filas TEST %s" % len(df_data_test))
 # Contamos cantidad de datos null por cada columna
-print(len(df_data_train) - df_data_train.count())
+# print(len(df_data_train) - df_data_train.count())
 #print(len(df_data_test) - df_data_test.count())
 
 
@@ -70,13 +71,28 @@ y_train = df_y_train.values
 
 X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, random_state=10, test_size=0.3)
 
+# ------------------------------------Sin preprocesamiento--------------------------------------
 # Hacer un For para que vaya tomando distintos valores en max_depth.
 clf = RandomForestClassifier(max_depth=3, random_state=0)
 clf.fit(X_train, y_train.ravel())
 
 y_hat = clf.predict(X_test)
 
-#print(y_hat)
-print(accuracy_score(y_true = y_test, y_pred = y_hat))
-print(metrics.classification_report(y_test, y_hat, target_names=["PassengerId","Survived"]))
+print("---------Sin preprocesamiento----------")
+print("Aciertos: ", accuracy_score(y_true=y_test, y_pred=y_hat))
+print(metrics.classification_report(y_test, y_hat))
 
+# --------------------------------------Normalizacion--------------------------------------------
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Hacer un For para que vaya tomando distintos valores en max_depth.
+clf = RandomForestClassifier(max_depth=3, random_state=0)
+clf.fit(X_train_scaled, y_train.ravel())
+
+y_hat = clf.predict(X_test_scaled)
+
+print("---------Con normalizacion----------")
+print("Aciertos: ", accuracy_score(y_true=y_test, y_pred=y_hat))
+print(metrics.classification_report(y_test, y_hat))
