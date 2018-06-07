250eeebd10310a0c1eaed0fffe824edd994c7eac
GasparCarraro <gaspar_carraro@hotmail.com>
2018-06-07 16:34:25 -0300

    Hyperparametros

--- TP 4 Random Forest.py
+++ TP 4 Random Forest.py
@@ -5,10 +5,12 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import metrics
+import matplotlib.pyplot as plt
 
 df_data_train = pd.read_csv('train.csv', header=0)
 #df_data_test = pd.read_csv('test.csv', header=0)
 
+accuracy = np.zeros(100)
 
 # Cantidad total de filas
 # print("Cantidad total de filas TRAIN %s" % len(df_data_train))
@@ -73,26 +75,67 @@ X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, random_sta
 
 # ------------------------------------Sin preprocesamiento--------------------------------------
 # Hacer un For para que vaya tomando distintos valores en max_depth.
-clf = RandomForestClassifier(max_depth=3, random_state=0)
+
+mejor_k = 0
+mejor_tasa_aciertos = 0
+
+for k in range(1, 101):
+    clf = RandomForestClassifier(n_estimators=k)
+    clf.fit(X_train, y_train.ravel())
+
+    y_hat = clf.predict(X_test)
+
+    tasa_aciertos = accuracy_score(y_true=y_test, y_pred=y_hat)
+
+    accuracy[k-1] = tasa_aciertos
+
+    if tasa_aciertos > mejor_tasa_aciertos:
+        mejor_tasa_aciertos = tasa_aciertos
+        mejor_k = k
+
+clf = RandomForestClassifier(n_estimators=mejor_k)
 clf.fit(X_train, y_train.ravel())
 
 y_hat = clf.predict(X_test)
 
 print("---------Sin preprocesamiento----------")
-print("Aciertos: ", accuracy_score(y_true=y_test, y_pred=y_hat))
+print("Aciertos para %s arboles: " % mejor_k, mejor_tasa_aciertos)
+
 print(metrics.classification_report(y_test, y_hat))
 
-# --------------------------------------Normalizacion--------------------------------------------
-scaler = preprocessing.StandardScaler().fit(X_train)
-X_train_scaled = scaler.transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+# -------------------------------------Normalizacion--------------------------------------
+mean_X_train = np.mean(X_train)
+stdv_X_train = np.std(X_train)
+X_train_scaled = (X_train - mean_X_train) / stdv_X_train
+X_test_scaled = (X_test - mean_X_train) / stdv_X_train
+
+mejor_k = 0
+mejor_tasa_aciertos = 0
+
+for k in range(1, 101):
+    clf = RandomForestClassifier(n_estimators=k)
+    clf.fit(X_train_scaled, y_train.ravel())
+
+    y_hat = clf.predict(X_test_scaled)
+
+    tasa_aciertos = accuracy_score(y_true=y_test, y_pred=y_hat)
+
+    accuracy[k-1] = tasa_aciertos
+
+    if tasa_aciertos > mejor_tasa_aciertos:
+        mejor_tasa_aciertos = tasa_aciertos
+        mejor_k = k
 
 # Hacer un For para que vaya tomando distintos valores en max_depth.
-clf = RandomForestClassifier(max_depth=3, random_state=0)
+clf = RandomForestClassifier(n_estimators=mejor_k)
 clf.fit(X_train_scaled, y_train.ravel())
 
 y_hat = clf.predict(X_test_scaled)
 
-print("---------Con normalizacion----------")
-print("Aciertos: ", accuracy_score(y_true=y_test, y_pred=y_hat))
+print("---------Con normalizacion manual----------")
+print("Aciertos para %s arboles: " % mejor_k, mejor_tasa_aciertos)
 print(metrics.classification_report(y_test, y_hat))
+
+# plt.cla()
+plt.bar(range(100), accuracy)
+plt.show()
